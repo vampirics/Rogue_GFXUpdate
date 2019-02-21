8d4ee2ba148e7c8f2ac3dd4dc5deae8df4e65b37
Stephane C <30958385+vampirics@users.noreply.github.com>
2019-02-21 15:48:26 -0500

    updated to match the latest updated from BlueMaxDX

--- Rogue_GFX_Update/src/fonts/Font5x7.cpp
+++ Rogue_GFX_Update/src/fonts/Font5x7.cpp
@@ -161,12 +161,13 @@ void Font5x7::printChar(const char c, const int8_t x, int8_t y) {
 
   if (idx > -1) {
     
-    if (_textColor == WHITE) {
-      Sprites::drawOverwrite(x, y, font_images, idx);
-    }
-    else {
-      Sprites::drawErase(x, y, font_images, idx);
-    }
+//    if (_textColor == WHITE) {
+      SpritesB::drawOverwrite(x, y, font_images, idx);
+//    }
+//    else {
+//      Sprites::drawOverwrite(x, y, back_ground, 0);
+//      SpritesB::drawErase(x, y, font_images, idx);
+//    }
 
   }
 
